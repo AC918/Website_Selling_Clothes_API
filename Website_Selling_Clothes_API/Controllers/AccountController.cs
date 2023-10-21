aeb3d529294776aa594c289feb686cb6af38f2b1
AC918 <102367792+AC918@users.noreply.github.com>
2023-10-21 15:04:00 +0700

    Add files via upload

---
+++ Website_Selling_Clothes_API/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APP_DATA.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Website_Selling_Clothes_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public WebsiteSellingClothes _context = new WebsiteSellingClothes();
+        // GET: api/<AccountController>
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return _context.accounts.ToList();
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("get-by-id")]
+        public Account GetAccountByID(Guid id)
+        {
+            return _context.accounts.Find(id);
+        }
+
+        // POST api/<AccountController>
+        [HttpPost("post-Account")]
+        public bool Post([FromBody] Guid id, string user, string password, string accountrole)
+        {
+            try
+            {
+                id = new Guid();
+                Account acc = new Account
+                {
+                    User = user,
+                    Password = password,
+                    RoleAccountID = accountrole,
+                };
+                _context.accounts.Add(acc);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        // PUT api/<AccountController>/5
+        [HttpPut("put-acc")]
+        public bool Put([FromBody] Account acc)
+        {
+            
+            try
+            {
+                var account = _context.accounts.FirstOrDefault(x => x.Id == acc.Id);
+                account.User = acc.User;
+                account.Password = acc.Password;
+                _context.accounts.Update(account);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return  false;
+            }
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("delete-account")]
+        public bool Delete(Guid id)
+        {
+            try
+            {
+                Account account = _context.accounts.Find(id);
+                _context.accounts.Remove(account);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ Website_Selling_Clothes_API/Controllers/BillController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APP_DATA.Models;
+using System.ComponentModel.DataAnnotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Website_Selling_Clothes_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillController : ControllerBase
+    {
+        public WebsiteSellingClothes _context = new WebsiteSellingClothes();
+        // GET: api/<BillController>
+        [HttpGet("get-all-products")]
+        public IEnumerable<Bill> Get()
+        {
+            return _context.bills.ToList();
+        }
+
+        // GET api/<BillController>/5
+        [HttpGet("-get-bill-id")]
+        public Bill Get(Guid id)
+        {
+            return _context.bills.Find(id);
+        }
+
+        // POST api/<BillController>
+        [HttpPost]
+        public bool Post([FromBody] Guid id, string productname, string discount, string user, string voucher)
+        {
+           
+            try
+            {
+                id = new Guid();
+                Bill bill = new Bill
+                {
+                    ProductName = productname,
+                    Discount = discount,
+                    UserID = user,
+                    VoucherID = voucher,
+                };
+                _context.bills.Add(bill);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        // PUT api/<BillController>/5
+        [HttpPut("put-bill")]
+        public bool Put( [FromBody] Bill bills)
+        {
+            Bill billfrDB = _context.bills.Find(bills.ID);
+            try
+            {
+                billfrDB.ProductName = bills.ProductName;
+                billfrDB.Discount = bills.Discount;
+                billfrDB.UserID = bills.UserID;
+                billfrDB.VoucherID = bills.VoucherID;
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        // DELETE api/<BillController>/5
+        [HttpDelete("delete-bill")]
+        public bool Delete(Guid id)
+        {
+            try
+            {
+                Bill bills = _context.bills.Find(id);
+                _context.bills.Remove(bills);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
